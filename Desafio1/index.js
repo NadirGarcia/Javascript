83d753458b0aa662450bc249782c49162800dd19
Nadir Garcia <nugarcia82@gmail.com>
2022-09-22 00:38:12 -0300

    correcciones primer desafio

--- Desafio1/index.js
+++ Desafio1/index.js
@@ -1,6 +1,7 @@
 const productos = [];
 const carrito = [];
 
+
 class Item{
     constructora(producto){
         this.nombre = producto.nombre;
@@ -44,55 +45,68 @@ productos.push(heusinkveldSps);
 
 
 
+function miCarrito(){
+    carrito.forEach(item => {
+        let mensaje = `
+            ${item.nombre} 
+            $ ${item.precio}
+        `
+        alert(mensaje);
+    })
+}
 
-//productos.forEach(item => console.log(item.nombre));
-
-/* let lg = productos.find(item => item === productos[1]);
-
-console.log(lg);
-
-function producto(item) {
-    switch (item) {
+function compra(comprar){
+    switch(comprar){
         case "1":
-            return productos[0];
-        case "2":
-            return productos[1];
-        case "3":
-            return productos[2];
-        default:
-            alert("Error");
+            carrito.push(logitechG29);
             break;
+        case "2":
+            carrito.push(trusthmasterT300);
+            break;
+        case "3":
+            carrito.push(fanatecCsl);
+            break;
+        case "4":
+            carrito.push(heusinkveldSps);
+            break;
+        default:
+            alert("Error")
+            break
     }
-} */
+}
+
 
 let bienvenida = prompt("Bienvenido a la tienda de NGsimracing \n 1. Volantes \n 2. Pedaleras \n 3. Salir" );
 
-while (bienvenida !== "3") {
-    if (bienvenida === "1") {
-        let volantes = productos.filter(item => item.categoria === "Volantes");
-        volantes.forEach(item =>{
-            let mensaje = `
-            Nombre: ${item.nombre} 
-            Precio: ${item.precio} 
-            Stock: ${item.stock}`;
-            
-            alert(mensaje);
-    })
-    } else if(bienvenida === "2"){
-        let pedaleras = productos.filter(item => item.categoria === "Pedaleras");
-        pedaleras.forEach(item =>{
-        let mensaje2 = `
+while (bienvenida !== "3"){
+if (bienvenida === "1") {
+    let volantes = productos.filter(item => item.categoria === "Volantes");
+    volantes.forEach(item =>{
+        let mensaje = `
         Nombre: ${item.nombre} 
         Precio: ${item.precio} 
         Stock: ${item.stock}`;
-        
-        alert(mensaje2);
-    })
-    } else{
-        break;
-    }
+        alert(mensaje);
+})
+let comprar = prompt("Seleccione el producto \n 1. Logitech G29 \n 2. Trusthmaster T300 \n 3. Fanatec Csl");
+compra(comprar);
+} else if(bienvenida === "2"){
+    let pedaleras = productos.filter(item => item.categoria === "Pedaleras");
+    pedaleras.forEach(item =>{
+    let mensaje = `
+    Nombre: ${item.nombre} 
+    Precio: ${item.precio} 
+    Stock: ${item.stock}`;
+    alert(mensaje);
+})
+comprar = prompt("Seleccione el producto \n 4. Heusinkveld SIM PEDALS SPRINT");
+compra(comprar);
+} else if(bienvenida === "3"){
+    alert("Gracias por visitar nuestra tienda");
+}else{
+    alert("Ustes a ingresado una opcion incorrecta")
+}
+bienvenida = prompt("Bienvenido a la tienda de NGsimracing \n 1. Volantes \n 2. Pedaleras \n 3. Salir" )
 }
 
-if (bienvenida === "3") {
-    alert("Gracias por visitar nuestra tienda");
-}
+miCarrito();
