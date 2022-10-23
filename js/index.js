4124c89a97f14fee0f8768083a47335951f17537
Nadir Garcia <nugarcia82@gmail.com>
2022-10-23 19:44:50 -0300

    avances trabajo final

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ const botonBuscar = document.getElementById("btnBuscar");
 const botonQuitarFiltro = document.getElementById("botonQuitarFiltro");
 const volantes = document.getElementById("volantes");
 const pedaleras = document.getElementById("pedaleras");
+const shifters = document.getElementById("shifters");
+const handbrake = document.getElementById("frenoDeMano");
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 const contenedorCarrito = document.getElementById("contenedorCarrito");
 const contenedorTienda = document.getElementById("contenedorTienda");
@@ -21,27 +23,47 @@ const imprimirProductos = async () => {
 }
 let productos = await imprimirProductos();
 
+
 //RENDERIZACION DE PRODUCTOS
 let renderizarProductos = () => {
     productos.forEach((producto) => {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
-                    <img class = "card__imagen" src="${producto.imagen}" alt="">
-                    <div class= "card__info">
-                        <h3> ${producto.nombre}</h3>
-                        <span> Precio: $ ${producto.precio}</span>
-                        <p> Stock: ${producto.stock}</p>
-                        <button id = ${producto.id} class = "btn" >Agregar</button>
-                    </div>
-                `;
+            <div class = "card__imagen">    
+                <img class = "card__imagen--img" src="${producto.imagen}" alt="imagen">
+            </div>   
+            <div class= "card__info">
+                <h3> ${producto.nombre}</h3>
+                <span> Precio: $ ${producto.precio}</span>
+                <span id = "stock${producto.id}">${producto.stock}</span>
+                <button id = ${producto.id} class = "btnAgregar" >Agregar</button>
+            </div>
+        `;
         contenedorTienda.append(card);
         const botonAgregar = document.getElementById(producto.id);
         botonAgregar.addEventListener("click", () => agregarCarrito(producto));
-});
+        let stock = document.getElementById(`stock${producto.id}`);
+        if(producto.stock < 1){
+            stock.innerHTML = "SIN STOCK";
+            stock.className = "sin__stock";
+            botonAgregar.className = "btnAgregar__off"
+        }else if(producto.stock < 3){
+            stock.innerHTML = "STOCK BAJO";
+            stock.className = "stock__bajo";
+        }else{
+            stock.innerHTML = "DISPONIBLE";
+            stock.className = "stock__disponible";
+        }      
+    });
 };
 
 renderizarProductos()
+
+
+
+
+
 
 /* //CONSTRUCTOR PRODUCTOS
 class Item {
@@ -73,12 +95,7 @@ const agregarCarrito = (producto) => {
     localStorage.setItem("enCarrito", JSON.stringify(carrito));
 };
 
-/* const sumarCantidadCarrito = () => {
-    let modificarProducto = carrito.find(existe => existe.id === producto.id);
-    if(modificarProducto !== null){
 
-    }
-}; */
 
 //RENDERIZACION DE PRODUCTO EN CARRITO
 const imprimirCarrito = () => {
@@ -94,19 +111,42 @@ const imprimirCarrito = () => {
             <button id ="sumar${item.id}" class = "btn__sumres">+</button>
             <button id ="restar${item.id}" class = "btn__sumres">-</button>
             <td><span> $ ${item.precio}</span></td>
-            <td><button id = ${item.id} class = "btn__eliminar"> <img src ="../resources/trash.png"></button></td>
+            <td><button id = "eliminar${item.id}" class = "btn__eliminar"> <img src ="../resources/trash.png"></button></td>
             `;
         contenedorCarrito.append(itemCarrito);
+        const botonEliminar = document.getElementById(`eliminar${item.id}`);
+        botonEliminar.addEventListener("click", () => eliminarItem(item));
+        const btnSumar = document.getElementById(`sumar${item.id}`);
+        btnSumar.addEventListener("click", () => sumarCantidadCarrito(item));
+        const btnRestar = document.getElementById(`restar${item.id}`);
+        btnRestar.addEventListener("click", () => restarCantidadCarrito(item));
         totalCompra();
         contadorCarrito();
-        const botonEliminar = document.getElementById(item.id);
-        botonEliminar.addEventListener("click", () => eliminarItem(item));
-        const btnSumar = document.getElementById(`sumar${item.id}`)
-        btnSumar.addEventListener("click", () => sumarCantidadCarrito);
-        const btnRestar = document.getElementById(`restar${item.id}`)
-        btnRestar.addEventListener("click", () => restarCantidadCarrito);
     });
 };
+
+const sumarCantidadCarrito = (item) => {
+    let productoOriginal = productos.find(existe => existe.id === item.id)
+    let productoModificar = carrito.find(existe => existe.id === item.id)
+    productoModificar.precio = productoModificar.precio + productoOriginal.precio
+    productoModificar.cantidad = productoModificar.cantidad + 1
+    imprimirCarrito()
+    localStorage.setItem("enCarrito", JSON.stringify(carrito));
+};
+
+const restarCantidadCarrito = (item) => {
+    let productoOriginal = productos.find(existe => existe.id === item.id);
+    let productoModificar = carrito.find(existe => existe.id === item.id);
+    let indice = carrito.indexOf(productoModificar);
+    productoModificar.precio = productoModificar.precio - productoOriginal.precio
+    productoModificar.cantidad = productoModificar.cantidad - 1
+    productoModificar.cantidad < 1 ? carrito.splice(indice, 1) : false;
+    imprimirCarrito()
+    totalCompra();
+    contadorCarrito();
+    localStorage.setItem("enCarrito", JSON.stringify(carrito));
+}
+
 
 //CONTADOR PRODUCTOS EN CARRITO
 const contadorCarrito = () => {
@@ -181,7 +221,7 @@ let renderizarLS = carritoLS ? true : false;
 renderizarLS ? carrito = carritoLS : false;
 imprimirCarrito();
 
-//FILTRAR POR VOLANTES
+//CATEGORIA VOLANTES
 volantes.addEventListener("click", () => {
     let volantesEncontrados = productos.filter(producto => producto.categoria === "Volantes");
     contenedorTienda.innerHTML = "";
@@ -189,21 +229,35 @@ volantes.addEventListener("click", () => {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
-        <img class = "card__imagen" src="${producto.imagen}" alt="">
+        <div class = "card__imagen">    
+            <img class = "card__imagen--img" src="${producto.imagen}" alt="imagen">
+        </div>   
         <div class= "card__info">
-        <h3> ${producto.nombre}</h3>
-        <span> Precio: $ ${producto.precio}</span>
-        <p> Stock: ${producto.stock}</p>
-        <button id = ${producto.id} class = "btn" >Agregar</button>
+            <h3> ${producto.nombre}</h3>
+            <span> Precio: $ ${producto.precio}</span>
+            <span id = "stock${producto.id}">${producto.stock}</span>
+            <button id = ${producto.id} class = "btnAgregar" >Agregar</button>
         </div>
         `;
         contenedorTienda.append(card);
         const botonAgregar = document.getElementById(producto.id);
         botonAgregar.addEventListener("click", () => agregarCarrito(producto));
+        let stock = document.getElementById(`stock${producto.id}`);
+        if(producto.stock < 1){
+            stock.innerHTML = "SIN STOCK";
+            stock.className = "sin__stock";
+            botonAgregar.className = "btnAgregar__off"
+        }else if(producto.stock < 3){
+            stock.innerHTML = "STOCK BAJO";
+            stock.className = "stock__bajo";
+        }else{
+            stock.innerHTML = "DISPONIBLE";
+            stock.className = "stock__disponible";
+        }      
     });
 });
 
-//FILTRAR POR PEDALERAS
+//CATEGORIA PEDALERAS
 pedaleras.addEventListener("click",() => {
     let pedalesEncontrados = productos.filter(producto => producto.categoria === "Pedaleras");
     contenedorTienda.innerHTML = "";
@@ -211,37 +265,119 @@ pedaleras.addEventListener("click",() => {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
-        <img class = "card__imagen" src="${producto.imagen}" alt="">
+        <div class = "card__imagen">    
+            <img class = "card__imagen--img" src="${producto.imagen}" alt="imagen">
+        </div>   
         <div class= "card__info">
-        <h3> ${producto.nombre}</h3>
-        <span> Precio: $ ${producto.precio}</span>
-        <p> Stock: ${producto.stock}</p>
-        <button id = ${producto.id} class = "btn" >Agregar</button>
+            <h3> ${producto.nombre}</h3>
+            <span> Precio: $ ${producto.precio}</span>
+            <span id = "stock${producto.id}">${producto.stock}</span>
+            <button id = ${producto.id} class = "btnAgregar" >Agregar</button>
         </div>
         `;
         contenedorTienda.append(card);
         const botonAgregar = document.getElementById(producto.id);
         botonAgregar.addEventListener("click", () => agregarCarrito(producto));
+        let stock = document.getElementById(`stock${producto.id}`);
+        if(producto.stock < 1){
+            stock.innerHTML = "SIN STOCK";
+            stock.className = "sin__stock";
+            botonAgregar.className = "btnAgregar__off"
+        }else if(producto.stock < 3){
+            stock.innerHTML = "STOCK BAJO";
+            stock.className = "stock__bajo";
+        }else{
+            stock.innerHTML = "DISPONIBLE";
+            stock.className = "stock__disponible";
+        }      
+    });
+});
+
+//CATEGORIA HANBRAKE
+handbrake.addEventListener("click",() => {
+    let pedalesEncontrados = productos.filter(producto => producto.categoria === "Handbrake");
+    contenedorTienda.innerHTML = "";
+    pedalesEncontrados.forEach(producto => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <div class = "card__imagen">    
+            <img class = "card__imagen--img" src="${producto.imagen}" alt="imagen">
+        </div>   
+        <div class= "card__info">
+            <h3> ${producto.nombre}</h3>
+            <span> Precio: $ ${producto.precio}</span>
+            <span id = "stock${producto.id}">${producto.stock}</span>
+            <button id = ${producto.id} class = "btnAgregar" >Agregar</button>
+        </div>
+        `;
+        contenedorTienda.append(card);
+        const botonAgregar = document.getElementById(producto.id);
+        botonAgregar.addEventListener("click", () => agregarCarrito(producto));
+        let stock = document.getElementById(`stock${producto.id}`);
+        if(producto.stock < 1){
+            stock.innerHTML = "SIN STOCK";
+            stock.className = "sin__stock";
+            botonAgregar.className = "btnAgregar__off"
+        }else if(producto.stock < 3){
+            stock.innerHTML = "STOCK BAJO";
+            stock.className = "stock__bajo";
+        }else{
+            stock.innerHTML = "DISPONIBLE";
+            stock.className = "stock__disponible";
+        }      
+    });
+});
+
+//CATEGORIA SHIFTERS
+shifters.addEventListener("click",() => {
+    let pedalesEncontrados = productos.filter(producto => producto.categoria === "Shifters");
+    contenedorTienda.innerHTML = "";
+    pedalesEncontrados.forEach(producto => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <div class = "card__imagen">    
+            <img class = "card__imagen--img" src="${producto.imagen}" alt="imagen">
+        </div>   
+        <div class= "card__info">
+            <h3> ${producto.nombre}</h3>
+            <span> Precio: $ ${producto.precio}</span>
+            <span id = "stock${producto.id}">${producto.stock}</span>
+            <button id = ${producto.id} class = "btnAgregar" >Agregar</button>
+        </div>
+        `;
+        contenedorTienda.append(card);
+        const botonAgregar = document.getElementById(producto.id);
+        botonAgregar.addEventListener("click", () => agregarCarrito(producto));
+        let stock = document.getElementById(`stock${producto.id}`);
+        if(producto.stock < 1){
+            stock.innerHTML = "SIN STOCK";
+            stock.className = "sin__stock";
+            botonAgregar.className = "btnAgregar__off"
+        }else if(producto.stock < 3){
+            stock.innerHTML = "STOCK BAJO";
+            stock.className = "stock__bajo";
+        }else{
+            stock.innerHTML = "DISPONIBLE";
+            stock.className = "stock__disponible";
+        }      
     });
 });
 
 
+//BUSCADOR  
+botonBuscar.addEventListener("click", () => {
+    let buscado = inputBuscar.value.toLowerCase()
+    let encontrado = productos.filter(item => item.categoria.includes(buscado))
+    inputBuscar.value = "";
+    encontrado.length === 0 ? console.log("Producto no encontrado"): console.log(encontrado);
+});
 
-//QUITAR FILTROS
+
+/* //QUITAR FILTROS
 botonQuitarFiltro.addEventListener("click", () => {
     contenedorTienda.innerHTML = "";
     renderizarProductos();
 });
-
-
-
-
-//BUSCADOR  
-/* const buscador = (item) => {
-    console.log(item);
-    let encontrado = productos.filter(producto => producto.categoria === "Volantes")
-    console.log(encontrado);
-    inputBuscar.value = "";
-} 
-
-botonBuscar.addEventListener("click", () => buscador(inputBuscar.value)); */
+ */
