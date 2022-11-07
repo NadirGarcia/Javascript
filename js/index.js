ed5fba76ae5e62371dc933f3d39c707649fd7688
Nadir Garcia <nugarcia82@gmail.com>
2022-11-07 18:18:23 -0300

    trabajo final

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
 let carrito = [];
 let carritoLS = JSON.parse(localStorage.getItem("enCarrito"));
+let usuarios = [];
+let usuariosLS = JSON.parse(localStorage.getItem("usuarioLS"));
+let usuarioSS = JSON.parse(sessionStorage.getItem("usuarioEnLinea"));
 const inputBuscar = document.getElementById("inputBuscar");
 const botonBuscar = document.getElementById("btnBuscar");
 const botonQuitarFiltro = document.getElementById("botonQuitarFiltro");
 const volantes = document.getElementById("volantes");
+const volantesFooter = document.getElementById("volantesFooter");
 const pedaleras = document.getElementById("pedaleras");
-const shifters = document.getElementById("shifters");
+const pedalerasFooter = document.getElementById("pedalerasFooter");
 const handbrake = document.getElementById("frenoDeMano");
-const finalizarCompra = document.getElementById("finalizarCompra");
+const handbrakeFooter = document.getElementById("frenoDeManoFooter");
+const shifters = document.getElementById("shifters");
+const shiftersFooter = document.getElementById("shiftersFooter");
+const btnComprar = document.getElementById("btnComprar");
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 const contenedorCarrito = document.getElementById("contenedorCarrito");
 const contenedorTienda = document.getElementById("contenedorTienda");
+const contenedorLogin = document.getElementById("contenedorLogin");
 const btnCarrito = document.getElementById("btnCarrito");
-const usuario = document.getElementById("inputUsuario");
+const email = document.getElementById("inputEmail");
 const pass = document.getElementById("inputPass");
 const btnLogin = document.getElementById("btnLogin");
-const btnRegistro = document.getElementById("btnRegistro");
+const btnRegistrarse = document.getElementById("btnRegistro");
+const contenedorCompra = document.getElementById("contenedorCompra");
 
 //TRAER ARRAY DE productos.json
 const imprimirProductos = async () => {
@@ -25,10 +34,9 @@ const imprimirProductos = async () => {
         return data;
     } catch {
         console.log("Error");
-    }
-}
+    };
+};
 let productos = await imprimirProductos();
-
 
 //RENDERIZACION DE PRODUCTOS
 let renderizarProductos = (array) => {
@@ -51,20 +59,20 @@ let renderizarProductos = (array) => {
         const botonAgregar = document.getElementById(producto.id);
         botonAgregar.addEventListener("click", () => agregarCarrito(producto));
         let stock = document.getElementById(`stock${producto.id}`);
-        if(producto.stock < 1){
+        if (producto.stock < 1) {
             stock.innerHTML = "SIN STOCK";
             stock.className = "sin__stock";
             botonAgregar.className = "btnAgregar__off"
-        }else if(producto.stock < 3){
+        } else if (producto.stock < 3) {
             stock.innerHTML = "STOCK BAJO";
             stock.className = "stock__bajo";
-        }else{
+        } else {
             stock.innerHTML = "DISPONIBLE";
             stock.className = "stock__disponible";
-        }      
+        };
     });
 };
-renderizarProductos(productos)
+renderizarProductos(productos);
 
 //AGREGAR PRODUCTOS AL CARRITO
 const agregarCarrito = (producto) => {
@@ -88,7 +96,7 @@ const imprimirCarrito = () => {
     contenedorCarrito.innerHTML = "";
     carrito.forEach(item => {
         const itemCarrito = document.createElement("tr");
-        itemCarrito.className = "carrito"
+        itemCarrito.classList = "carrito";
         itemCarrito.innerHTML = `
             <td><img class = "carrito__imagen" src="${item.imagen}" alt=""></td>
             <td><h5>${item.nombre}</h5></td>
@@ -113,14 +121,13 @@ const imprimirCarrito = () => {
     });
 };
 
-
 //BOTON SUMAR CARRITO
 const sumarCantidadCarrito = (item) => {
-    let productoOriginal = productos.find(existe => existe.id === item.id)
-    let productoModificar = carrito.find(existe => existe.id === item.id)
-    productoModificar.precio = productoModificar.precio + productoOriginal.precio
-    productoModificar.cantidad = productoModificar.cantidad + 1
-    imprimirCarrito()
+    let productoOriginal = productos.find(existe => existe.id === item.id);
+    let productoModificar = carrito.find(existe => existe.id === item.id);
+    productoModificar.precio = productoModificar.precio + productoOriginal.precio;
+    productoModificar.cantidad = productoModificar.cantidad + 1;
+    imprimirCarrito();
     localStorage.setItem("enCarrito", JSON.stringify(carrito));
 };
 
@@ -129,15 +136,15 @@ const restarCantidadCarrito = (item) => {
     let productoOriginal = productos.find(existe => existe.id === item.id);
     let productoModificar = carrito.find(existe => existe.id === item.id);
     let indice = carrito.indexOf(productoModificar);
-    productoModificar.precio = productoModificar.precio - productoOriginal.precio
-    productoModificar.cantidad = productoModificar.cantidad - 1
+    productoModificar.precio = productoModificar.precio - productoOriginal.precio;
+    productoModificar.cantidad = productoModificar.cantidad - 1;
     productoModificar.cantidad < 1 ? carrito.splice(indice, 1) : false;
     imprimirCarrito();
     contadorCarrito();
     totalCompra();
     botonCarrito();
     localStorage.setItem("enCarrito", JSON.stringify(carrito));
-}
+};
 
 //CONTADOR PRODUCTOS EN CARRITO
 const contadorCarrito = () => {
@@ -201,7 +208,7 @@ vaciarCarrito.addEventListener("click", () => {
             totalCompra();
             contadorCarrito();
             botonCarrito();
-            localStorage.clear();
+            localStorage.removeItem("enCarrito");
             Swal.fire(
                 'Tu carrito fue vaciado',
             )
@@ -210,28 +217,25 @@ vaciarCarrito.addEventListener("click", () => {
 });
 
 //BOTON CARRITO
-
 const botonCarrito = () => {
-    if(carrito.length !== 0){
+    if (carrito.length !== 0) {
+        contenedorCarrito.classList = "carrito";
         imprimirCarrito();
-    }else{
-        contenedorCarrito.innerHTML = `
-        <h2>Su carrito esta vacio!</h2>
-        `;
-        contenedorCarrito.className = "carrito__vacio";
-    }
+    } else {
+        contenedorCarrito.innerHTML = `<h2>Su carrito esta vacio!</h2>`;
+        contenedorCarrito.classList = "carrito__vacio";
+    };
 };
+btnCarrito.addEventListener("click", botonCarrito);
 
-btnCarrito.addEventListener("click", botonCarrito)
-
-
-finalizarCompra.addEventListener("click", () => {
+//CONTINUAR COMPRA
+const comprar = () => {
     contenedorTienda.innerHTML = "";
-    contenedorTienda.className = "contenedor__compra";
+    contenedorTienda.classList = "contenedor__compra";
     contenedorTienda.innerHTML = `<p class="contenedor__compra--titulo">Carrito de compras</p>`;
     carrito.forEach(item => {
-        const itemCarrito = document.createElement("tr");
-        itemCarrito.className = "carrito__compra"
+        const itemCarrito = document.createElement("div");
+        itemCarrito.className = "carrito__compra";
         itemCarrito.innerHTML = `
             <img class = "carrito__imagen" src="${item.imagen}" alt="">
             <h5>${item.nombre}</h5>
@@ -239,14 +243,49 @@ finalizarCompra.addEventListener("click", () => {
             `;
         contenedorTienda.append(itemCarrito);
     });
-})
+    const footerCompra = document.createElement("div");
+    footerCompra.className = "compra__footer";
+    footerCompra.innerHTML = `
+        <span class="compra__footer--total">TOTAL COMPRA: $ ${carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)}</span>
+        <button id ="btnFinalizarCompra" class = "btn compra__footer--btn">Finalizar la Compra</button>
+        `;
+        contenedorTienda.append(footerCompra);
+        const btnFinalizarCompra = document.getElementById("btnFinalizarCompra");
+        btnFinalizarCompra.addEventListener("click", terminarCompra);
+};
 
+//BOTON CONTINUAR COMPRA
+btnComprar.addEventListener("click", () => {
+    usuarioSS = JSON.parse(sessionStorage.getItem("usuarioEnLinea"));
+    if(carrito.length !== 0 && usuarioSS){
+        comprar();
+    }else if(carrito.length === 0){
+        contenedorTienda.innerHTML = `
+        <p>No tiene productos en su carrito</p>
+        <button id="continuarComprando" class="contenedor__mensaje--btn">Continuar Comprando</button>
+        `;
+        contenedorTienda.className = "contenedor__mensaje";
+    }else{
+        contenedorTienda.innerHTML = "Debe estar logueado para finalizar la compra";
+        contenedorTienda.className = "contenedor__mensaje";
+    };
+    const continuarComprando = document.getElementById("continuarComprando")
+    continuarComprando.addEventListener("click", () => {window.location = "http://127.0.0.1:5500/index.html"});
+});
 
-
-
-/* const compra = () => {
-
-}; */
+//FINALIZAR LA COMPRA
+const terminarCompra = () => {
+    carrito.length = 0;
+    contadorCarrito();
+    totalCompra();
+    localStorage.removeItem("enCarrito");
+    contenedorTienda.innerHTML = "";
+    contenedorTienda.classList = "finalizacion__compra";
+    contenedorTienda.innerHTML = `
+        <p>FELICITACIONES!! tu compra se realizo con exito</p>
+        <p>Gracias por confiar en NGSimracing!!!</p>
+    `;
+}
 
 //RENDERIZACION DE CARRITO DESDE LOCALSTORAGE
 let renderizarLS = carritoLS ? true : false;
@@ -255,88 +294,134 @@ imprimirCarrito();
 
 
 //CATEGORIA VOLANTES
-volantes.addEventListener("click", () => {
+const btnVolantes = () => {
     let volantesEncontrados = productos.filter(producto => producto.categoria === "volantes");
     contenedorTienda.innerHTML = "";
     renderizarProductos(volantesEncontrados);
-}); 
+};
+volantes.addEventListener("click", btnVolantes);
 
-const volantesFooter = document.getElementById("volantesFooter");
-volantesFooter.addEventListener("click", () => {
+//CATEGORIA VOLANTES FOOTER
+const btnVolantesFooter = () => {
     let volantesEncontrados = productos.filter(producto => producto.categoria === "volantes");
     contenedorTienda.innerHTML = "";
     renderizarProductos(volantesEncontrados);
-}); 
+};
+volantesFooter.addEventListener("click", btnVolantesFooter);
 
 //CATEGORIA PEDALERAS
-pedaleras.addEventListener("click",() => {
+const btnPedaleras = () => {
     let pedalesEncontrados = productos.filter(producto => producto.categoria === "pedaleras");
     contenedorTienda.innerHTML = "";
     renderizarProductos(pedalesEncontrados);
-});
+};
+pedaleras.addEventListener("click", btnPedaleras);
 
-const pedalerasFooter = document.getElementById("pedalerasFooter");
-pedalerasFooter.addEventListener("click",() => {
+//CATEGORIA PEDALERAS FOOTER
+const btnPedalerasFooter = () => {
     let pedalesEncontrados = productos.filter(producto => producto.categoria === "pedaleras");
     contenedorTienda.innerHTML = "";
     renderizarProductos(pedalesEncontrados);
-});
+};
+pedalerasFooter.addEventListener("click", btnPedalerasFooter);
 
 //CATEGORIA HANBRAKE
-handbrake.addEventListener("click",() => {
+const btnHandbrake = () => {
     let handbrakeEncontrados = productos.filter(producto => producto.categoria === "handbrake");
     contenedorTienda.innerHTML = "";
     renderizarProductos(handbrakeEncontrados);
-});
+};
+handbrake.addEventListener("click", btnHandbrake);
 
-const handbrakeFooter = document.getElementById("frenoDeManoFooter");
-handbrakeFooter.addEventListener("click",() => {
+//CATEGORIA HANBRAKE FOOTER
+const btnHandbrakeFooter = () => {
     let handbrakeEncontrados = productos.filter(producto => producto.categoria === "handbrake");
     contenedorTienda.innerHTML = "";
     renderizarProductos(handbrakeEncontrados);
-});
-
+};
+handbrakeFooter.addEventListener("click", btnHandbrakeFooter);
 
 //CATEGORIA SHIFTERS
-shifters.addEventListener("click",() => {
+const btnShifters = () => {
     let shiftersEncontrados = productos.filter(producto => producto.categoria === "shifters");
     contenedorTienda.innerHTML = "";
     renderizarProductos(shiftersEncontrados);
-});
+};
+shifters.addEventListener("click", btnShifters);
 
-const shiftersFooter = document.getElementById("shiftersFooter");
-shiftersFooter.addEventListener("click",() => {
+//CATEGORIA SHIFTER FOOTER
+const btnShiftersFooter = () => {
     let shiftersEncontrados = productos.filter(producto => producto.categoria === "shifters");
     contenedorTienda.innerHTML = "";
     renderizarProductos(shiftersEncontrados);
-});
-
+};
+shiftersFooter.addEventListener("click", btnShiftersFooter);
 
 //BUSCADOR INPUT
 let buscador = () => {
     let buscado = inputBuscar.value.toLowerCase();
     inputBuscar.value = "";
-    if((buscado !== "") && (buscado.length > 1)){
+    if ((buscado !== "") && (buscado.length > 1)) {
         contenedorTienda.innerHTML = "";
         let encontrado = productos.filter(item => (item.categoria.includes(buscado)) || (item.marca.includes(buscado)));
         encontrado.length === 0 ? (
             (contenedorTienda.innerHTML = "Producto no encontrado")
             (contenedorTienda.className = "contenedor__mensaje")
-            ): 
+        ) :
             renderizarProductos(encontrado);
-    }else{
+    } else {
         contenedorTienda.innerHTML = "Ingrese el producto que desea buscar!!";
         contenedorTienda.className = "contenedor__mensaje";
     };
 };
-
 inputBuscar.addEventListener("change", buscador);
 
+//REGISTRAR USUARIO LOCALSTORAGE
+const generarUsuario = () => {
+    class Usuario{
+        constructor(correo,password){
+        this.correo = correo;
+        this.password = password;
+        }
+    };
+    const usuarioNuevo = new Usuario(email.value, pass.value);
+    usuarios.push(usuarioNuevo);
+    localStorage.setItem("usuarioLS", JSON.stringify(usuarios));
+};
+btnRegistrarse.addEventListener("click", generarUsuario);
 
+//LOGUEAR USUARIO SESSIONSTORAGE
+const login = () => {
+    let usuarioEncontrado = usuariosLS.find(user => user.correo === email.value);
+    if(usuarioEncontrado.password !== pass.value){
+        Swal.fire('Contraseña incorrecta');
+    }else{
+        email.value = "";
+        pass.value = "";
+        contenedorLogin.innerHTML = `Hola ${usuarioEncontrado.correo}`;
+        const cerrarSession = document.createElement("button");
+        cerrarSession.className = "div__login--btn";
+        cerrarSession.innerHTML = "Cerrar sesion";
+        contenedorLogin.append(cerrarSession);
+        cerrarSession.addEventListener("click", sessionClear);
+        sessionStorage.setItem("usuarioEnLinea",JSON.stringify(usuarioEncontrado));
+        scroll(0,0);
+    };
+}; 
+btnLogin.addEventListener("click", login);
 
-/* //QUITAR FILTROS
-botonQuitarFiltro.addEventListener("click", () => {
-    contenedorTienda.innerHTML = "";
-    renderizarProductos();
-});
- */
+//BOTON CERRAR SESION
+const sessionClear = () => {
+    contenedorLogin.innerHTML = "";
+    sessionStorage.clear();
+};
+
+//RENDERIZAR LOGIN DESDE SESSIONSTORAGE
+if(usuarioSS){
+    contenedorLogin.innerHTML = `Hola ${usuarioSS.correo}`;
+    const cerrarSession = document.createElement("button");
+    cerrarSession.className = "div__login--btn";
+    cerrarSession.innerHTML = "Cerrar sesion";
+    contenedorLogin.append(cerrarSession);
+    cerrarSession.addEventListener("click", sessionClear);
+};
